2f33baf18180b0b280b75e8c2719c047921db5dd
K.Stopa <k.stopa86@gmail.com>
2026-01-28 10:07:51 +0100

    feat: first basic implementation that creates a templated entry

---
+++ src/main.rs
@@ -0,0 +1,87 @@
+use chrono::{Duration, Local};
+use std::fs::File;
+use std::io::{self, Write};
+
+fn ask_question(question: &str) -> String {
+    // Promt a question for the user and returns the given answer trimed.
+    let mut answer = String::new();
+    print!("{question} ");
+    io::stdout().flush().unwrap(); // Make sure prompt shows before input
+    io::stdin().read_line(&mut answer).unwrap();
+    answer.trim().to_string()
+}
+
+fn write_entry(
+    // TODO Create struct
+    // TODO i18n and translations
+    at_date: String,
+    a_how_day: String,
+    a_remarkable: String,
+    a_best: String,
+    a_worst: String,
+    a_new: String,
+    a_learn: String,
+    a_breifing: String,
+) -> std::io::Result<()> {
+    let mut file = File::create(format!("./data/{at_date}.md"))?;
+
+    writeln!(file, "# {}\n", at_date)?;
+
+    writeln!(file, "## Stats\n")?;
+    writeln!(file, "* **Nota**: {}/5\n\n", a_how_day)?;
+
+    writeln!(file, "## Q&A\n")?;
+    writeln!(file, "* **Lo más destacado**: {}", a_remarkable)?;
+    writeln!(file, "* **Lo mejor**: {}", a_best)?;
+    writeln!(file, "* **Lo peor**: {}", a_worst)?;
+    writeln!(file, "* **He conocidos**: {}", a_new)?;
+    writeln!(file, "* **He aprendido**: {}\n", a_learn)?;
+
+    // TODO: format text to max 80 chars per line
+    writeln!(file, "## Resumen\n")?;
+    writeln!(file, "{}", a_breifing)?;
+
+    Ok(())
+}
+
+fn main() {
+    // TODO enable to set the date from args
+    let yesterday_date = Local::now() - Duration::days(1);
+    let at_date = yesterday_date.format("%Y-%m-%d");
+    println!("Welcome to the diary on {at_date}");
+    println!("---------------------------------");
+
+    let answer_how_day = ask_question("How was your day? (1-5):");
+
+    let answer_remarkable = ask_question("What was the most remarkable:");
+
+    let answer_best = ask_question("What was the best:");
+
+    let answer_worst = ask_question("What was the worst:");
+
+    let answer_who_new = ask_question("Who you've met?");
+
+    let answer_learn = ask_question("What new you've leanred?");
+
+    let answer_breif = ask_question("Day briefing");
+
+    let resp = write_entry(
+        at_date.to_string(),
+        answer_how_day,
+        answer_remarkable,
+        answer_best,
+        answer_worst,
+        answer_who_new,
+        answer_learn,
+        answer_breif,
+    );
+    match resp {
+        Ok(_) => {
+            println!("Writing new entry at data/{}.md", at_date);
+            io::stdout().flush().unwrap();
+        }
+        Err(e) => {
+            println!("Error {} writing a new entry at data/{}.md", e, at_date);
+        }
+    }
+}
